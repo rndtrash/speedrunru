ce128726a00d709bb1810358290e0da78c3d6b84
Ivan Abramov <ivabramov16@gmail.com>
2025-02-22 23:20:29 +0300

    #1 SP1 - Настройка gradle, подключение к базе данных

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,10 @@
 rootProject.name = "speedrun"
+
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.spring") version kotlinVersion
+        kotlin("plugin.jpa") version kotlinVersion
+    }
+}
