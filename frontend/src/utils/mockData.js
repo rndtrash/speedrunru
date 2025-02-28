2b5910453b5d9678f987b97e6a8f547293097aa5
SomeClade <someclade0@gmail.com>
2025-02-28 22:22:36 +0300

    добавление страницы ошибки, работает на 404. Обновление иконки меню. МЕГА МОКИ. Добавлена страница игры, добавлена сортировка внутри таблицы.

--- frontend/src/utils/mockData.js
+++ frontend/src/utils/mockData.js
@@ -1,24 +1,25 @@
-export const speedrunMockData = Array.from({ length: 50 }, () => {
-    const games = [
-        { game: 'MiSide', icon: '/assets/gameIcons/miside.png' },
-        { game: 'Risk of Rain 2', icon: '/assets/gameIcons/riskofrain2.png' },
-        { game: 'Hollow Knight', icon: '/assets/gameIcons/hollowknight.png' },
-    ];
-    const gameChoice = games[Math.floor(Math.random() * games.length)];
+import { gameInfoMock } from './gameInfoMock.js';
+// МММ мок на моке
 
-    const nicknames = [
-        'Speedster',
-        'Lightning',
-        'Flash',
-        'Rocket',
-        'Ninja',
-        'Samurai',
-        'LegendX',
-        'Turbo',
-        'Blaze',
-        'Phantom',
-    ];
-    const user = nicknames[Math.floor(Math.random() * nicknames.length)] +
+const nicknames = [
+    'Speedster',
+    'Lightning',
+    'Flash',
+    'Rocket',
+    'Ninja',
+    'Samurai',
+    'LegendX',
+    'Turbo',
+    'Blaze',
+    'Phantom',
+];
+
+export const speedrunMockData = Array.from({ length: 50 }, () => {
+    const gameChoice =
+        gameInfoMock[Math.floor(Math.random() * gameInfoMock.length)];
+
+    const user =
+        nicknames[Math.floor(Math.random() * nicknames.length)] +
         Math.floor(Math.random() * 1000);
 
     const place = Math.floor(Math.random() * 100) + 1;
@@ -31,7 +32,8 @@ export const speedrunMockData = Array.from({ length: 50 }, () => {
     const date = new Date(2023, 7, randomDay, randomHour, randomMinute).toISOString();
 
     return {
-        game: gameChoice.game,
+        id: gameChoice.id,
+        game: gameChoice.name,
         icon: gameChoice.icon,
         place,
         user,
