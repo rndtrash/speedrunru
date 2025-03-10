fbbf1a23b391c22297b050d168ca06db6d2cc688
Sc00taloo <v4nyapop0v12@gmail.com>
2025-03-10 05:02:00 +0300

    Костыль, который не помог
    Пытался запустить проект через папку ru.speedrun.speedrun, но ничего не получилось((( . Если выкинуть SpeedrunApplication.kt  из папки, то всё работает

--- src/main/kotlin/ru/speedrun/speedrun/SpeedrunApplication.kt
+++ src/main/kotlin/ru/speedrun/speedrun/SpeedrunApplication.kt
@@ -1,9 +1,11 @@
 package ru.speedrun.speedrun
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+//import org.springframework.context.annotation.ComponentScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+//@ComponentScan(basePackages = ["ru.speedrun"])
 class SpeedrunApplication
 
 fun main(args: Array<String>) {
