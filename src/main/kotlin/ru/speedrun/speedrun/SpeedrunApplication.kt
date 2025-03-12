f09d87416424cbf60c913f5c2efd9972156e4fd5
Sc00taloo <v4nyapop0v12@gmail.com>
2025-03-12 18:50:18 +0300

    Микро фикс

--- src/main/kotlin/ru/speedrun/speedrun/SpeedrunApplication.kt
+++ src/main/kotlin/ru/speedrun/speedrun/SpeedrunApplication.kt
@@ -1,11 +1,9 @@
 package ru.speedrun.speedrun
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-//import org.springframework.context.annotation.ComponentScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-//@ComponentScan(basePackages = ["ru.speedrun"])
 class SpeedrunApplication
 
 fun main(args: Array<String>) {
